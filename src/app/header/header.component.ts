8a6768336a47d4c8539559f704e01b08bd7f5fc0
Sinouhé <sinouhes@gmail.com>
2023-09-29 23:55:07 +0200

    feat: home button

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   competitionName = 'Trailer Boys Competition';
+
+  constructor(private router: Router) {}
+
+  navigateHome() {
+    this.router.navigate(['/home']);
+  }
 }
