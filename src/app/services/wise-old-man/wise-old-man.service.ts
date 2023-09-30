9d5291e8dc52b60a00369a265bf014dc697c715a
Sinouhé <sinouhes@gmail.com>
2023-09-30 05:17:05 +0200

    feat: added API key

--- src/app/services/wise-old-man/wise-old-man.service.ts
+++ src/app/services/wise-old-man/wise-old-man.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { CompetitionDetails } from '../../models/competition.model';
 import { firstValueFrom } from 'rxjs'
@@ -9,6 +9,7 @@ const API_BASE = 'https://api.wiseoldman.net/v2';
 const COMPETITION_API = '/competitions/:id';
 const PLAYER_GAINS_API = '/players/:username/gained';
 const PLAYER_UPDATE_API = '/players/:username';
+const API_KEY = 'to0fe0auxjiypmnexhwme6eg';
 
 @Injectable({
   providedIn: 'root'
@@ -17,19 +18,29 @@ export class WiseOldManService {
   constructor(private http: HttpClient) { }
 
   getCompetition(id: number, metric?: string): Promise<CompetitionDetails> {
-    const options = metric ? {params: new HttpParams().set('metric', metric)} : {};
+    const options = {
+      headers: new HttpHeaders().set('x-api-key', API_KEY), 
+      params: {}
+    };
+    if(metric) options.params = new HttpParams().set('metric', metric);
     const url = API_BASE + COMPETITION_API.replace(':id', id.toString());
     return firstValueFrom(this.http.get<CompetitionDetails>(url, options));
   }
 
   getPlayerDetails(playerName: string, startDate: string, endDate: string): Promise<PlayerGains> {
-    const options = {params: new HttpParams().set('startDate', startDate).set('endDate', endDate)}
+    const options = {
+      headers: new HttpHeaders().set('x-api-key', API_KEY), 
+      params: new HttpParams().set('startDate', startDate).set('endDate', endDate),
+    }
     const url = API_BASE + PLAYER_GAINS_API.replace(':username', playerName);
     return firstValueFrom(this.http.get<PlayerGains>(url, options));
   }
 
   updatePlayer(playerName: string): Promise<PlayerDetails> {
+    const options = {
+      headers: new HttpHeaders().set('x-api-key', API_KEY), 
+    };
     const url = API_BASE + PLAYER_UPDATE_API.replace(':username', playerName); 
-    return firstValueFrom(this.http.post<PlayerDetails>(url, {}));
+    return firstValueFrom(this.http.post<PlayerDetails>(url, {}, options));
   }
 }
